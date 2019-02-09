21eb6cc14c89f055e9716e56cf2704993fc430fa
Sam Kopp <sam@kopp.email>
2019-02-09 18:47:30 -0500

    clean

--- hack/osc_server.py
+++ hack/osc_server.py
@@ -5,43 +5,40 @@ from liblo import *
 import time
 
 
+def printer(thingy):
+    print(thingy)
+
 class MuseServer(ServerThread):
 
-    # Listen for messages on port 5002
-    print("here")
+    # Listen for messages on port 5001
     def __init__(self):
         ServerThread.__init__(self, 5001)
 
 
     # Receive accelrometer data
-    @make_method('/muse/acc', 'fff')
-    def acc_callback(self, path, args):
-        acc_x, acc_y, acc_z = args
-        print(f"{path} {acc_x} {acc_y} {acc_z}")
+    @make_method('/muse/elements/experimental/concentration', 'f')
+    def concentration_callback(self, path, args):
+        concentration = args
+        printer(concentration)
+        #print(f"{path} {concentration}")
 
 
     # Receive EEG data
-    @make_method('/muse/eeg', 'ffff')
-    def eeg_callback(self, path, args):
-        l_ear, l_forehead, r_forehead, r_ear = args
-        print(f"{path} {l_ear} {l_forehead} {r_forehead} {r_ear}")
+    #@make_method('/muse/eeg', 'ffff')
+    #def eeg_callback(self, path, args):
+    #    l_ear, l_forehead, r_forehead, r_ear = args
+    #    print(f"{path} {l_ear} {l_forehead} {r_forehead} {r_ear}")
 
 
     # Handle unexpected messages
     @make_method(None, None)
     def fallback(self, path, args, types, src):
-        print(f"Unknown message \
-                \n\t Source: '{src.url}' \
-                \n\t Address: '{path}' \
-                \n\t Types: '{types} ' \
-                \n\t Payload: '{args}'")
+        pass
 
 server = MuseServer()
 server.start()
-print(server.get_protocol())
+#print(server.get_protocol())
 
 if __name__ == "__main__":
     while 1:
         time.sleep(1)
-
-
